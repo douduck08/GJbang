20719dcb3a59f1481ccfa737496f973a672238ac
TZ <peng86david@gmail.com>
2018-01-27 09:44:22 +0800

    我學會用GIT惹

---
+++ Assets/Script/box_flow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_flow : MonoBehaviour {
+    public Rigidbody rb;
+    public int force = 5;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(new Vector3(-((Screen.width) / 2 - Input.mousePosition.x) / 100, 0, 0) * force);
+	}
+}
