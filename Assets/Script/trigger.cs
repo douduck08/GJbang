19c6899977523ab34de8394b0a4d8d52dfc1a84f
ZigerMa <zigerma@gmail.com>
2018-01-28 13:56:34 +0800

    ZM
    unitil upload

---
+++ Assets/Script/trigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour {
+    public Obstacle_behavior[] obs;
+    public GameObject end;
+
+    int stage = 0;
+
+    void Update() {
+        Debug.Log(end.transform.position.x);
+        if (stage == 0 && end.transform.position.x < -37)
+        {
+            for (int i = 0; i < obs.Length; i++)
+            {
+                obs[i].enabled = false;
+            }
+            stage += 1;
+        } 
+
+    }
+}
---
+++ Assets/colider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colider : MonoBehaviour {
+
+    public GameObject Player;
+
+
+    void OntriggerEnter(colider other)
+    {
+       
+            Destroy(Player);
+        
+    }
+}
