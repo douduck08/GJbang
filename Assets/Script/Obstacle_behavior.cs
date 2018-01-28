74ffb4079045325b330beaf4fa805242d94e2aa1
TZ <peng86david@gmail.com>
2018-01-28 15:51:26 +0800

    FINALLY DONE

--- Assets/Script/Obstacle_behavior.cs
+++ Assets/Script/Obstacle_behavior.cs
@@ -3,16 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle_behavior : MonoBehaviour {
-    public GameObject obstacle;
-	// Use this for initialization
-	void Start () {
-
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-        obstacle.transform.position += new Vector3(-0.05f, 0, 0);
-      
+        this.transform.position += new Vector3(-0.05f, 0, 0);
 	}
 }
--- Assets/Script/trigger.cs
+++ Assets/Script/trigger.cs
@@ -1,23 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class trigger : MonoBehaviour {
-    public Obstacle_behavior[] obs;
-    public GameObject end;
 
-    int stage = 0;
+    public int stage = 0;
+
+    public trigger[] stages;
+    public Transform[] pages;
 
     void Update() {
-        Debug.Log(end.transform.position.x);
-        if (stage == 0 && end.transform.position.x < -10)
+        if (stage == 0 && this.transform.position.x < -10)
         {
-            for (int i = 0; i < obs.Length; i++)
-            {
-                obs[i].enabled = false;
-            }
-            stage += 1;
-        } 
+            Ending();
+        }
 
+        if (stage == 1 && this.transform.position.x < -39)
+        {
+            this.GetComponent<Obstacle_behavior>().enabled = false;
+            pages[0].gameObject.SetActive(true);
+            stages[0].enabled = false;
+        }
+        if (stage == 2 && this.transform.position.x < -67)
+        {
+            this.GetComponent<Obstacle_behavior>().enabled = false;
+            pages[1].gameObject.SetActive(true);
+            stages[1].enabled = false;
+        }
+        if (stage == 3 && this.transform.position.x < -80)
+        {
+            this.GetComponent<Obstacle_behavior>().enabled = false;
+            pages[2].gameObject.SetActive(true);
+            stages[2].enabled = false;
+        }
+    }
+
+    public void StartStage (int stage)
+    {
+        pages[stage - 2].gameObject.SetActive(false);
+        stages[stage - 1].enabled = true;
+        stages[stage - 1].GetComponent<Obstacle_behavior>().enabled = true;
+    }
+
+    public void Ending ()
+    {
+        SceneManager.LoadScene("menu");
     }
 }
